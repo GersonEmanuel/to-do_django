7d85857502bc9fbaf5991b3a968ac490908508ef
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-10-05 19:13:47 -0300

    cbv

--- todos/views.py
+++ todos/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from .models import Todo
+from  django.views.generic import ListView, CreateView
+
 
 
 # Create your views here.
-def todo_list(request):
-    todos = Todo.objects.all()
-    return render(request, "todos/todo_list.html", {'todos': todos})
+class TodoListView(ListView):
+    model = Todo
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", 'deadline']
