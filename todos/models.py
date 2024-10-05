a7f3f1b3768443d264acbf4fe5f9587e2fef5256
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-10-05 16:59:18 -0300

    using bootstrap

--- todos/models.py
+++ todos/models.py
@@ -4,6 +4,6 @@ from django.db import models
 # Create your models here.
 class Todo(models.Model):
     title = models.CharField(max_length=100, null=False, blank=False)
-    data_created = models.DateField(auto_now_add=True, null=False, blank=False)
+    data_created = models.DateTimeField(auto_now_add=True, null=False, blank=False)
     deadline = models.DateField(null=False, blank=False)
     finished_data = models.DateField(null=True)
